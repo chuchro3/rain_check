1519c1777707c3d268f6df581ae150ebb79c88b8
Robert Chuchro <rchuchro@groupon.com>
2012-06-29 18:27:59 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RainCheck::Application.config.secret_token = '16b055acc156bd7d5ae04d85e0c7377efd6fef9e40c53e72f6690c28a0811927b7547ec889d4ec5b8439306678c9f385fac0536f9120ca8eaedb5135df5afc4c'
